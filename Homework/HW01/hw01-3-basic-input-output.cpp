f9d1dee8fbcb198f529415af35e12905ed3187b3
Biwane20 <biwbew20@gmail.com>
2023-06-29 17:54:38 +0700

    HW3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -1,15 +1,24 @@
-/*
-    เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
-    
-    Test case:
-        First Name: Aomsin
-        Last Name: AomMoney
-    Output:
-        Aomsin AomMoneys TC, RMUTL, Chiang Mai, Thailand
+#include <stdio.h>
 
-    Test case:
-        First Name: ABC
-        Last Name: DEF
-    Output:
-        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
-*/
+int main() {
+    char FirstName[50],LastName[50];
+    char FirstName1[50],LastName1[50];
+
+    printf("First Name:");
+    scanf("%s",&FirstName);
+
+    printf("Last Name:");
+    scanf("%s",&LastName);
+
+    printf("%s %s TC, RMUTL, Chiang Mai, Thailand\n",FirstName,LastName);
+    
+    printf("First Name1:");
+    scanf("%s",&FirstName1);
+
+    printf("Last Name1:");
+    scanf("%s",&LastName1);
+
+    printf("%s %s TC, RMUTL, Chiang Mai, Thailand\n",FirstName1,LastName1);
+    
+    return 0;
+}
