0174b8aacb78239dd43445554939348abdf8c995
Biwane20 <biwbew20@gmail.com>
2023-06-29 17:58:46 +0700

    HW13

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -2,8 +2,6 @@
 
 int main() {
     char FirstName[50],LastName[50];
-    char FirstName1[50],LastName1[50];
-
     printf("First Name:");
     scanf("%s",&FirstName);
 
@@ -11,14 +9,6 @@ int main() {
     scanf("%s",&LastName);
 
     printf("%s %s TC, RMUTL, Chiang Mai, Thailand\n",FirstName,LastName);
-    
-    printf("First Name1:");
-    scanf("%s",&FirstName1);
-
-    printf("Last Name1:");
-    scanf("%s",&LastName1);
-
-    printf("%s %s TC, RMUTL, Chiang Mai, Thailand\n",FirstName1,LastName1);
-    
+  
     return 0;
 }
